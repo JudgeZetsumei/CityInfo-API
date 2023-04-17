f95032d92a209b82ce271b8b0fb6933b34fbc2eb
Jonathan Hunter <jonnyhunter450@hotmail.co.uk>
2023-04-17 18:02:40 +0100

    Add Serilog logger

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
