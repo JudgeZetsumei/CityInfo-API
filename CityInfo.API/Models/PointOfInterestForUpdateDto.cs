91dc6c8b3e7e058d03c7ac337227397a3e3a0ab2
Jonathan Hunter <jonnyhunter450@hotmail.co.uk>
2023-04-17 16:02:37 +0100

    Add Model PointOfInterestForUpdateDto

---
+++ CityInfo.API/Models/PointOfInterestForUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForUpdateDto
+    {
+        // ToDo: Look into using FLuentValidation instead, as this will provide a separation of concerns instead of applying validation logic to the Model.
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+    }
+}
